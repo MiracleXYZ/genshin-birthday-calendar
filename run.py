3028ad6ea1151fb52a7ed30da81fb137e74ffc6f
MiracleXYZ <xieyuzhang_xyz@outlook.com>
2026-01-05 19:38:03 +0800

    Fix bugs. Use uv to manage packages. Remove local ical dependency.

--- run.py
+++ run.py
@@ -4,13 +4,11 @@ from pathlib import Path
 from typing import Any, Literal
 
 from httpx import AsyncClient
-from pydantic import BaseModel, PositiveInt
-
 from ical.calendar import Calendar
 from ical.calendar_stream import IcsCalendarStream
 from ical.event import Event, EventStatus
-from ical.parsing.property import ParsedProperty
 from ical.types import Frequency, Recur, Uri
+from pydantic import BaseModel, PositiveInt
 
 LANGUAGE: Literal[
     "chs",
@@ -67,15 +65,13 @@ async def main():
         prodid="-//Karako//Genshin Impact Birthday Calendar 0.1.0//CHS",
         version="2.0",
         extras=[
-            ParsedProperty(name="name", value="原神生日日历", params=None),
-            ParsedProperty(name="x-wr-calname", value="原神生日日历", params=None),
-            ParsedProperty(name="timezone-id", value="Asia/Shanghai", params=None),
-            ParsedProperty(name="x-wr-timezone", value="Asia/Shanghai", params=None),
-            ParsedProperty(name="X-PUBLISHED-TTL", value="PT1H", params=None),
-            ParsedProperty(name="x-wr-caldesc", value="提瓦特全角色生日日历", params=None),
-            ParsedProperty(
-                name="REFRESH-INTERVAL;VALUE=DURATION", value="PT1H", params=None
-            ),
+            {"name": "name", "value": "原神生日日历"},
+            {"name": "x-wr-calname", "value": "原神生日日历"},
+            {"name": "timezone-id", "value": "Asia/Shanghai"},
+            {"name": "x-wr-timezone", "value": "Asia/Shanghai"},
+            {"name": "X-PUBLISHED-TTL", "value": "PT1H"},
+            {"name": "x-wr-caldesc", "value": "提瓦特全角色生日日历"},
+            {"name": "REFRESH-INTERVAL;VALUE=DURATION", "value": "PT1H"},
         ],
     )
     if (
@@ -85,7 +81,7 @@ async def main():
     for data in [
         v
         for _, v in json_data["data"]["items"].items()
-        if v["name"] != "旅行者" and not v.get("beta", False)
+        if v["birthday"][0] != 0 and not v.get("beta", False)
     ]:
         new_json_data = await request(
             f"https://gi.yatta.moe/api/v2/{LANGUAGE}/avatarFetter/{data['id']}"
